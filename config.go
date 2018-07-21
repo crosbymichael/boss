4c23297ea71d89b0fda979407d2ad635d4834971
Michael Crosby <crosbymichael@gmail.com>
2018-07-21 09:12:23 -0400

    Update resource settings

--- config.go
+++ config.go
@@ -5,13 +5,22 @@ type Config struct {
 	Image     string     `toml:"image"`
 	Resources *Resources `toml:"resources"`
 	Network   Network    `toml:"network"`
+	Mounts    []Mount    `toml:"mounts"`
 }
 
 type Resources struct {
 	CPU    float64 `toml:"cpu"`
 	Memory int     `toml:"memory"`
+	Score  int     `toml:"score"`
 }
 
 type Network struct {
 	Host bool `toml:"host"`
+}
+
+type Mount struct {
+	Type        string   `toml:"type"`
+	Source      string   `toml:"source"`
+	Destination string   `toml:"destination"`
+	Options     []string `toml:"options"`
 }
--- run.go
+++ run.go
@@ -9,11 +9,13 @@ import (
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
 	"github.com/containerd/containerd/cmd/ctr/commands/content"
+	"github.com/containerd/containerd/containers"
 	"github.com/containerd/containerd/contrib/apparmor"
 	"github.com/containerd/containerd/contrib/seccomp"
 	"github.com/containerd/containerd/defaults"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/oci"
+	cni "github.com/containerd/go-cni"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/urfave/cli"
 )
@@ -24,6 +26,10 @@ var runCommand = cli.Command{
 	Action: func(clix *cli.Context) error {
 		var config Config
 		if _, err := toml.DecodeFile(clix.Args().First(), &config); err != nil {
+			return err
+		}
+		networking, err := cni.New()
+		if err != nil {
 			return err
 		}
 		ctx := namespaces.WithNamespace(context.Background(), clix.GlobalString("namespace"))
@@ -53,6 +59,20 @@ var runCommand = cli.Command{
 		if config.Network.Host {
 			opts = append(opts, oci.WithHostHostsFile, oci.WithHostResolvconf, oci.WithHostNamespace(specs.NetworkNamespace))
 		}
+		if config.Resources != nil {
+			opts = append(opts, withResources())
+		}
+		for _, cm := range config.Mounts {
+			opts = append(opts, oci.WithMounts([]specs.Mount{
+				{
+					Type:        cm.Type,
+					Source:      cm.Source,
+					Destination: cm.Destination,
+					Options:     cm.Options,
+				},
+			}),
+			)
+		}
 		logpath := filepath.Join(clix.GlobalString("log-path"), config.ID)
 		container, err := client.NewContainer(
 			ctx,
@@ -70,13 +90,51 @@ var runCommand = cli.Command{
 		fmt.Printf("created container %s with logpath %s\n", config.ID, logpath)
 		task, err := container.NewTask(ctx, cio.NullIO)
 		if err != nil {
+			container.Delete(ctx, containerd.WithSnapshotCleanup)
 			return err
 		}
+		if !config.Network.Host {
+			fmt.Println("using CNI networking...")
+			result, err := networking.Setup(config.ID, fmt.Sprintf("/proc/%d/ns/net", task.Pid()))
+			if err != nil {
+				task.Delete(ctx, containerd.WithProcessKill)
+				container.Delete(ctx, containerd.WithSnapshotCleanup)
+				return err
+			}
+			fmt.Printf("setup networking for %s with IP %#v\n", config.ID, result)
+		}
+
 		fmt.Println("starting container...")
 		if err := task.Start(ctx); err != nil {
+			task.Delete(ctx, containerd.WithProcessKill)
+			container.Delete(ctx, containerd.WithSnapshotCleanup)
 			return err
 		}
 		fmt.Printf("container %s started, have a great day!\n", config.ID)
 		return nil
 	},
+}
+
+type SpecOpts func(context.Context, Client, *containers.Container, *Spec) error
+
+func withResources(r *Resources) oci.SpecOpts {
+	return func(ctx context.Context, _ oci.Client, c *containers.Container, s *oci.Spec) error {
+		if r.Memory > 0 {
+			limit := r.Memory * 1024 * 1024
+			s.Linux.Resources.Memory = &specs.LinuxMemory{
+				Limit: &limit,
+			}
+		}
+		if r.CPU > 0 {
+			period := uint64(100000)
+			quota := int64(r.CPU * 100000.0)
+			s.Linux.Resources.CPU = &specs.LinuxCPU{
+				Quota:  &quota,
+				Period: &period,
+			}
+		}
+		if r.Score != 0 {
+			s.Linux.Resources.OOMScoreAdj = &r.Score
+		}
+	}
 }
