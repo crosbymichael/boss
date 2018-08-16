d5946e0a2fc07cb43b5b43d8b06bd8602c925837
Michael Crosby <crosbymichael@gmail.com>
2018-08-16 09:58:19 -0400

    Start agent server

--- agent.go
+++ agent.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"net"
+	"os"
+	"os/signal"
+
 	"github.com/crosbymichael/boss/agent"
+	"github.com/crosbymichael/boss/api/v1"
 	"github.com/crosbymichael/boss/config"
 	"github.com/crosbymichael/boss/system"
 	"github.com/urfave/cli"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var agentCommand = cli.Command{
 	Name:  "agent",
 	Usage: "run the boss agent",
 	Action: func(clix *cli.Context) error {
+		s := make(chan os.Signal, 32)
+		signal.Notify(s, os.Interrupt)
+
 		c, err := config.Load()
 		if err != nil {
 			return err
@@ -24,10 +35,29 @@ var agentCommand = cli.Command{
 		if err != nil {
 			return err
 		}
-		_, err = agent.New(c, client, store)
+		a, err := agent.New(c, client, store)
 		if err != nil {
 			return err
 		}
-		return nil
+		server := newServer()
+		v1.RegisterAgentServer(server, a)
+		go func() {
+			<-s
+			server.Stop()
+		}()
+		l, err := net.Listen("tcp", clix.GlobalString("agent"))
+		if err != nil {
+			return err
+		}
+		defer l.Close()
+		return server.Serve(l)
 	},
+}
+
+func newServer() *grpc.Server {
+	s := grpc.NewServer()
+
+	hs := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(s, hs)
+	return s
 }
