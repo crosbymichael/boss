f5092d769942737e82118af8a813fb4fa99d2213
Michael Crosby <crosbymichael@gmail.com>
2018-08-16 06:53:41 -0400

    Write ssh config with no password login

--- config/ssh.go
+++ config/ssh.go
@@ -7,11 +7,22 @@ import (
 	"path/filepath"
 
 	"github.com/containerd/containerd"
+	"github.com/crosbymichael/boss/systemd"
 	"github.com/urfave/cli"
 )
 
+const sshdConfig = `PermitRootLogin no
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem       sftp    /usr/lib/openssh/sftp-server`
+
 type SSH struct {
-	Admin string `toml:"admin"`
+	Admin  string `toml:"admin"`
+	Config bool   `toml:"sshd_config"`
 	// AuthorizedKeys string `toml:"authorized_keys"`
 }
 
@@ -20,6 +31,14 @@ func (m *SSH) Name() string {
 }
 
 func (m *SSH) Run(ctx context.Context, client *containerd.Client, clix *cli.Context) error {
+	if m.Config {
+		if err := ioutil.WriteFile("/etc/ssh/sshd_config", []byte(sshdConfig), 0644); err != nil {
+			return err
+		}
+		if err := systemd.Command(ctx, "restart", "sshd"); err != nil {
+			return err
+		}
+	}
 	home := filepath.Join(os.Getenv("HOME"), ".ssh", "authorized_keys")
 	if err := os.MkdirAll(filepath.Dir(home), 0775); err != nil {
 		return err
